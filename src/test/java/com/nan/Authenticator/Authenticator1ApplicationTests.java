edafb6cf58be0022166fafbbcdf7a79a32a82d04
NandhiniRakAS <nandinirakas>
2021-12-22 16:58:59 +0530

    created jsp

---
+++ src/test/java/com/nan/Authenticator/Authenticator1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nan.Authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Authenticator1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
