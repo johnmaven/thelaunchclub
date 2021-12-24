61405d9dbeb069513b565612ab742507093afe29
NandhiniRakAS <nandinirakas>
2021-12-24 15:09:01 +0530

    to merge with main

--- src/main/java/com/launchclub/HomeController.java
+++ src/main/java/com/launchclub/HomeController.java
@@ -1,16 +1,38 @@
 package com.launchclub;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-	@RequestMapping("home")
-	
-	public String home() {
-		System.out.print("hi");
-		return "home.jsp";
-	}
 
+	@GetMapping("/register")
+	public String showForm(Model model) {
+		Register register=new Register();
+		model.addAttribute("Register", register);
+		return "register_form";
+	}
+	@PostMapping("/register")
+	public String submitForm(@ModelAttribute("Register")Register register) {
+		System.out.println(register);
+		return "register_success";
+	}
+	 @GetMapping("/login") 
+	 public String loginform(Model model1) 
+	 { 	
+		 Login login=new Login();
+		 model1.addAttribute("Login",login);
+		 return "login_form";
+		 }
+	 @PostMapping("/login")
+		public String submitForm(@ModelAttribute("Login")Login login) {
+			System.out.println(login);
+			return "login_success";
+	
+	 }
 }
